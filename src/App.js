46f42af764d09db465b0703ab0e393563562eea7
chasegraffeo <chasegraffeo2@gmail.com>
2023-03-10 14:44:13 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+//import logo from './logo.svg';
+//import './App.scss';
+import './App.css';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <style type="text/css">
+        {`
+    .btn-flat {
+      background-color: purple;
+      color: grey;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+    }
+    `}
+      </style>
+
+        <p >
+          Hey.
+        </p>
+        <Button variant="flat" size="xxl" onClick={() => setIsOpen(true)}>
+        YOU
+      </Button>
+      {isOpen && (
+       <div>
+        <div>
+          A Bitch
+        </div>
+
+        <Button  variant="flat" size="xxl" onClick={() => setIsOpen(false)}>
+          Love You
+        </Button>
+       </div>
+         )}
+         
+
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*<style type="text/css">
+        {`
+    .btn-flat {
+      background-color: gray;
+      color: white;
+    }
+
+    .btn-xxl {
+      padding: 1rem 1.5rem;
+      font-size: 1.5rem;
+    }
+    `}
+      </style> */
