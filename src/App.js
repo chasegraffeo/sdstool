2e1540b171412b25dcde102363abdaaf8877620a
chasegraffeo <chasegraffeo2@gmail.com>
2023-03-10 16:51:20 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@
 import './App.css';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import { BsBank } from 'react-icons/bs';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="App">
+      
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">SDS Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link target="_blank" href="https://snventerprise.sharepoint.com/sites/OneSynovus">One Synovus</Nav.Link>
+            <Nav.Link target="_blank" href="https://synovusprodc.service-now.com/now/workspace/agent/home">Agent WorkSpace</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <header className="App-header">
       <style type="text/css">
         {`
@@ -25,7 +54,7 @@ function App() {
       </style>
 
         <p >
-          Hey.
+          Hey<BsBank />.
         </p>
         <Button variant="flat" size="xxl" onClick={() => setIsOpen(true)}>
         YOU
@@ -41,6 +70,14 @@ function App() {
         </Button>
        </div>
          )}
+
+
+
+
+<div class='wrap'>
+<iframe title="callBoard" src="https://synovus.clearviewportal.com/Admin/WallboardView.aspx?id=4eeaa788-2b49-4bba-ba18-b5bcb91f7c86" />
+</div>
+
          
 
        
@@ -66,3 +103,38 @@ export default App;
     }
     `}
       </style> */
+
+      /*import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample; */
